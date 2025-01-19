07370750eb4eec593c3e8907d754e09c966da791
oscar <oscar.sntcrz@gmail.com>
2025-01-19 18:03:20 -0300

    fix: quitar base URL para probar rutas relativas

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/appAlcancias/',
+  base: '',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
